08f9f7614ad47f5f394d2dbeeb08af24c83513c9
Rio Edwards <rioredwards@gmail.com>
2023-02-05 15:58:55 -0800

    IMAGES

--- src/components/RestaurantListItem.js
+++ src/components/RestaurantListItem.js
@@ -1,8 +1,11 @@
-export const RestaurantListItem = ({ name, rating }) => {
+export const RestaurantListItem = ({ name, rating, image_url }) => {
   return (
-    <p>
-      {name}
-      {Array(Math.floor(rating)).fill('⭐️')}
-    </p>
+    <>
+      <p>
+        {name}
+        {Array(Math.floor(rating)).fill('⭐️')}
+      </p>
+      <img src={image_url} style={{ width: '10rem' }} />
+    </>
   );
 };
