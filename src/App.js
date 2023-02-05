e62d69305fc162c0d47867b42e350a181fa47346
Rio Edwards <rioredwards@gmail.com>
2023-02-05 15:49:54 -0800

    WORKING

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { fetchBusinesses } from './services/yelp';
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [zip, setZip] = useState('');
+  const [search, setSearch] = useState('');
 
   // TODO -- add state for zip / search and add event listeners to the inputs
 
@@ -26,13 +28,30 @@ function App() {
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" />
+          <input
+            type="text"
+            placeholder="zip"
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label>Query:</label>
-          <input type="text" placeholder="Search..." />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
-        <button>Search</button>
+        <button
+          onClick={async () => {
+            const resp = await fetchBusinesses(zip, search);
+            setBusinesses(resp);
+          }}
+        >
+          Search
+        </button>
       </div>
       {loading && <div className="loader"></div>}
       {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
