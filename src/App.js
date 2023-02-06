03fd9cdb174aab0b5e9ffaebcf16729e1d72a01f
Rio Edwards <rioredwards@gmail.com>
2023-02-06 15:08:48 -0800

    Add styling with MaterialUI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { RestaurantListItem } from './components/RestaurantListItem';
 import { fetchBusinesses } from './services/yelp';
+import Button from '@mui/material/Button';
+import { theme } from './theme.js';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Box, Paper, ThemeProvider } from '@mui/material';
 
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [loading, setLoading] = useState(true);
   const [zip, setZip] = useState('');
   const [search, setSearch] = useState('');
+  console.log('businesses', businesses);
 
   // TODO -- add state for zip / search and add event listeners to the inputs
 
@@ -23,39 +31,42 @@ function App() {
   // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
 
   return (
-    <div className="App">
-      <h1>Alchemy Restaurant Finder</h1>
-      <div className="query-form">
-        <div className="form-control">
-          <label>Zip:</label>
-          <input
-            type="text"
-            placeholder="zip"
-            value={zip}
-            onChange={(e) => setZip(e.target.value)}
-          />
+    <ThemeProvider theme={theme}>
+      <Box sx={{ bgcolor: 'primary.background' }} className="App">
+        <h1>Alchemy Restaurant Finder</h1>
+        <div className="query-form">
+          <div className="form-control">
+            <label>Zip:</label>
+            <input
+              type="text"
+              placeholder="zip"
+              value={zip}
+              onChange={(e) => setZip(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label>Query:</label>
+            <input
+              type="text"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <Button
+            variant="contained"
+            onClick={async () => {
+              const resp = await fetchBusinesses(zip, search);
+              setBusinesses(resp);
+            }}
+          >
+            Search
+          </Button>
         </div>
-        <div className="form-control">
-          <label>Query:</label>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </div>
-        <button
-          onClick={async () => {
-            const resp = await fetchBusinesses(zip, search);
-            setBusinesses(resp);
-          }}
-        >
-          Search
-        </button>
-      </div>
-      {loading && <div className="loader"></div>}
-      {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
-    </div>
+        {loading && <div className="loader"></div>}
+        {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/RestaurantListItem.js
+++ src/components/RestaurantListItem.js
@@ -1,11 +1,49 @@
-export const RestaurantListItem = ({ name, rating, image_url }) => {
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Link,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { Restaurant, AccountCircle } from '@mui/icons-material';
+import './Restaurant.css';
+
+export const RestaurantListItem = ({ name, rating, image_url, location, categories }) => {
   return (
-    <>
-      <p>
-        {name}
-        {Array(Math.floor(rating)).fill('⭐️')}
-      </p>
-      <img src={image_url} style={{ width: '10rem' }} />
-    </>
+    <Card sx={{ width: 445, margin: 2 }}>
+      <ListItemButton />
+      <CardHeader
+        avatar={
+          <Avatar aria-label="utensils">
+            <Restaurant />
+          </Avatar>
+        }
+        title={
+          <Typography gutterBottom variant="h6" component="div">
+            <Link href="#" underline="hover">
+              {name}
+            </Link>
+          </Typography>
+        }
+        subheader={
+          <>
+            <ListItemText>{location.display_address.map((addr) => addr)}</ListItemText>
+            <ListItemText>{Array(Math.floor(rating)).fill('⭐️')}</ListItemText>
+          </>
+        }
+      />
+      <Divider />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {categories.map((cat, idx) => `${idx === 0 ? '' : ' • '}${cat.title}`)}
+        </Typography>
+      </CardContent>
+      <CardMedia component="img" height="194" image={image_url} alt={name} />
+    </Card>
   );
 };
